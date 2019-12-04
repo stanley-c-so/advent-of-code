6a5ab82e8ee18f06097e7a6a7b9d3637584d7626
stanley-c-so <45887253+stanley-c-so@users.noreply.github.com>
2019-12-04 06:25:31 -0500

    initial commit

---
+++ day1.js
@@ -0,0 +1,45 @@
+// DAY 1
+
+// DESCRIPTION_HERE
+
+// SWITCHING BETWEEN SOLUTIONS:
+const NAME_OF_ALGO_HERE = solution_1;
+
+function solution_1 (INPUT_HERE) {
+
+  // SOLUTION 1 [O(?) time, O(?) space]:
+  // description
+
+}
+
+// TEST CASES
+
+const test = require('./_test');
+const testNum = [1];
+let input, expected;
+const func = 'FUNCTION_NAME_HERE';
+const sortedFunc = (...args) => func(...args).sort();                   // used when the order of the output does not matter
+const modFunc = (...args) => func(...args) % 1000000007;                // used when the output is very large
+const lowestTest = 0 || 0;
+const highestTest = 0 || Infinity;
+
+// Test case 1
+input = {
+  ARG_1: 'INPUT_HERE',
+};
+expected = 'EXPECTED_HERE';
+test(func, input, expected, testNum, lowestTest, highestTest);
+
+// Test case 2
+input = {
+  ARG_1: 'INPUT_HERE',
+};
+expected = 'EXPECTED_HERE';
+test(func, input, expected, testNum, lowestTest, highestTest);
+
+// Test case 3
+input = {
+  ARG_1: 'INPUT_HERE',
+};
+expected = 'EXPECTED_HERE';
+test(func, input, expected, testNum, lowestTest, highestTest);
