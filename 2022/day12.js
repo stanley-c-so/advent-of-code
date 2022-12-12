16b3f26f38805f0719559868c7d2b88415993358
Stanley So <stanley.c.so@gmail.com>
2022-12-12 01:02:20 -0500

    Add second solution involving legit Queue class

--- 2022/day12.js
+++ 2022/day12.js
@@ -60,6 +60,8 @@ This path reaches the goal in only 29 steps, the fewest possible.
 What is the fewest steps required to move starting from any square with elevation a to the location that should get the best signal?
 */
 
+// ========== SOLUTION 1: BECAUSE JAVASCRIPT HAS NO BUILT-IN QUEUE CLASS, USE AN ARRAY WITH POINTER FOR O(1) DEQUEUE, AT COST OF SPACE
+
 function elevationBFS (part, inputStr, DEBUG = false) {
   const inputArr = inputStr.split('\r\n');
   // if (DEBUG) {
@@ -149,12 +151,139 @@ function elevationBFS (part, inputStr, DEBUG = false) {
   }
 }
 
+
+// ========== SOLUTION 2: SAME SOLUTION, BUT BUILD A LEGITIMATE QUEUE CLASS TO IMPROVE SPACE COMPLEXITY
+
+function elevationBFS2 (part, inputStr, DEBUG = false) {
+  const inputArr = inputStr.split('\r\n');
+  // if (DEBUG) {
+  //   console.log(inputArr[0]);
+  //   // console.log(inputArr[1]);
+  // }
+
+  class Node {
+    constructor(val, next = null) {
+      this.val = val;
+      this.next = next;
+    }
+  }
+
+  class Queue {
+    constructor(val) {
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
+      if (val) this.enqueue(val);
+    }
+    enqueue(val) {
+      const node = new Node(val);
+      if (this.isEmpty()) this.head = node;
+      else this.tail.next = node;
+      this.tail = node;
+      ++this.length;
+    }
+    dequeue() {
+      if (this.isEmpty()) return null;
+      const node = this.head;
+      this.head = this.head.next;
+      --this.length;
+      if (this.isEmpty()) this.tail = null;
+      return node;
+    }
+    isEmpty() {
+      return this.length === 0;
+    }
+  }
+
+  // INIT
+
+  // grid and grid traversal
+  const GRID = inputArr.map(str => str.split(''));
+  const H = GRID.length;
+  const W = GRID[0].length;
+  const DIRS = [ [0, +1], [0, -1], [+1, 0], [-1, 0] ];
+  const possibleStarts = [];                                                        // will be filled in during input parsing
+
+  // elevation map
+  const ALPHABET = 'abcdefghijklmnopqrstuvwxyz';
+  const HEIGHTS = { 'S': 0, 'E': 25 };
+  for (let i = 0; i < 26; ++i) HEIGHTS[ALPHABET[i]] = i;
+
+  // PARSE INPUT
+  let inputStartRow, inputStartCol;                                                 // will be discovered during input parsing
+  let endRow, endCol;                                                               // will be discovered during input parsing
+  for (let row = 0; row < H; ++row) {
+    for (let col = 0; col < W; ++col) {
+      if (GRID[row][col] === 'S') [inputStartRow, inputStartCol] = [row, col];      // PART 1: discover input start
+      if (GRID[row][col] === 'E') [endRow, endCol] = [row, col];                    // discover end
+      if (HEIGHTS[GRID[row][col]] === 0) possibleStarts.push([row, col]);           // PART 2: discover input start OR 'a'
+    }
+  }
+  if ([ inputStartRow, inputStartCol ].includes(undefined)) {                       // sanity check to make sure input start was found
+    throw 'ERROR: DID NOT DISCOVER COORDS OF INPUT START';
+  }
+  if ([ endRow, endCol ].includes(undefined)) {                                     // sanity check to make sure end was found
+    throw 'ERROR: DID NOT DISCOVER COORDS OF END';
+  }
+
+  // HELPER
+  function getBestPathStartingAt(startRow, startCol) {
+
+    // init
+    const Q = new Queue([startRow, startCol, 0]);
+    const visited = new Set();
+
+    // BFS
+    while (!Q.isEmpty()) {
+      const node = Q.dequeue();                                                     // dequeue
+      const [r, c, moves] = node.val;                                               // extract data from node
+
+      if (r === endRow && c === endCol) return moves;                               // END CONDITION: REACHED END
+
+      const serial = `${r},${c}`;                                                   // check against visited coords to prevent cycles
+      if (visited.has(serial)) continue;
+      visited.add(serial);
+
+      const height = HEIGHTS[GRID[r][c]];                                           // get height of current position
+
+      for (const [dy, dx] of DIRS) {                                                // attempt to visit 4 neighbors
+        const newY = r + dy;
+        const newX = c + dx;
+        if (
+          0 <= newY && newY < H && 0 <= newX && newX < W                            // target destination must be in bounds...
+          && HEIGHTS[GRID[newY][newX]] <= height + 1                                // ...and not more than 1 elevation higher
+        ) {
+          Q.enqueue([newY, newX, moves + 1]);
+        }
+      }
+    }
+
+    if (part === 1) throw 'ERROR: DID NOT FIND PATH';                               // PART 1: sanity check to make sure a path exists
+    else return Infinity;                                                           // PART 2: some starts may not have a valid path!
+  }
+
+  if (part === 1) {                                                                 // PART 1: GET BEST PATH STARTING AT INPUT START
+
+    return getBestPathStartingAt(inputStartRow, inputStartCol);
+
+  } else {                                                                          // PART 2: GET BEST PATH AMONG ALL POSSIBLE STARTS
+
+    let best = Infinity;
+    for (const [row, col] of possibleStarts) {
+      best = Math.min(best, getBestPathStartingAt(row, col));                       // if start has no path, function returns Infinity
+    }
+    return best;
+
+  }
+}
+
 // TEST CASES
 
 const test = require('./_test');
 const testNum = [1];
 let input, expected;
-const func = elevationBFS;
+// const func = elevationBFS;
+const func = elevationBFS2;
 const sortedFunc = (...args) => func(...args).sort();                   // used when the order of the output does not matter
 const modFunc = (...args) => func(...args) % 1000000007;                // used when the output is very large
 const skippedTests = new Set([  ]);
