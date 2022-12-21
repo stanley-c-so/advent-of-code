5aa1326f812d049d8aa8cf25b029c3eb3c0827e4
Stanley So <stanley.c.so@gmail.com>
2022-12-21 03:09:22 -0500

    Rename function

--- 2022/day21.js
+++ 2022/day21.js
@@ -62,7 +62,7 @@ What number do you yell to pass root's equality test?
 // OPTIONAL VARIABLES
 const DISPLAY_EXTRA_INFO = true;
 
-function dependencyChain (part, inputStr, DEBUG = false) {
+function dependencyChainAlgebra (part, inputStr, DEBUG = false) {
   const inputArr = inputStr.split('\r\n');
 
   // DATA STRUCTURES
@@ -253,7 +253,7 @@ function dependencyChain (part, inputStr, DEBUG = false) {
 const test = require('./_test');
 const testNum = [1];
 let input, expected;
-const func = dependencyChain;
+const func = dependencyChainAlgebra;
 const sortedFunc = (...args) => func(...args).sort();                   // used when the order of the output does not matter
 const modFunc = (...args) => func(...args) % 1000000007;                // used when the output is very large
 const skippedTests = new Set([  ]);
