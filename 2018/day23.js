d93d5e656323af8114302a1f3165a713144289b8
Stanley So <stanley.c.so@gmail.com>
2023-02-05 08:44:30 -0500

    Remove skipped test

--- 2018/day23.js
+++ 2018/day23.js
@@ -274,7 +274,7 @@ const func = findCoordCoincidingWithMostOctahedra;
 const sortedFunc = (...args) => func(...args).sort();                   // used when the order of the output does not matter
 const modFunc = (...args) => func(...args) % 1000000007;                // used when the output is very large
 const skippedTests = new Set([  ]);
-const lowestTest = 3;
+const lowestTest = 0;
 const highestTest = 0;
 
 const fs = require('fs');
