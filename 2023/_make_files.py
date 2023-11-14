2694924d88a5fd1c0664b32d78358bd1501cae06
Stanley So <stanley.c.so@gmail.com>
2023-11-14 00:13:50 -0500

    Add 2023 files for python

---
+++ 2023/_make_files.py
@@ -0,0 +1,15 @@
+# LIBRARIES
+import pathlib
+import os
+
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+for n in range(1, 26):
+  FILENAME = f"day{n}-input.txt"
+  INPUT_PATH = CURRENT_DIR + '\\' + FILENAME
+  try:
+    open(INPUT_PATH, 'r').read()
+    print(f"{FILENAME} already exists")
+  except:
+    open(INPUT_PATH, 'w+')
+    print(f"{FILENAME} created")
