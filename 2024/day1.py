d7ad9e5d8ec1626a87039b3e5c977221e0d38361
Stanley So <stanley.c.so@gmail.com>
2024-12-01 00:14:49 -0500

    Initialize 2024, and day 1

---
+++ 2024/day1.py
@@ -0,0 +1,199 @@
+"""
+
+--- Day 1: Historian Hysteria ---
+
+The Chief Historian is always present for the big Christmas sleigh launch, but nobody has seen him in months! Last anyone heard, he was visiting locations that are historically significant to the North Pole; a group of Senior Historians has asked you to accompany them as they check the places they think he was most likely to visit.
+
+As each location is checked, they will mark it on their list with a star. They figure the Chief Historian must be in one of the first fifty places they'll look, so in order to save Christmas, you need to help them get fifty stars on their list before Santa takes off on December 25th.
+
+Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
+
+You haven't even left yet and the group of Elvish Senior Historians has already hit a problem: their list of locations to check is currently empty. Eventually, someone decides that the best place to check first would be the Chief Historian's office.
+
+Upon pouring into the office, everyone confirms that the Chief Historian is indeed nowhere to be found. Instead, the Elves discover an assortment of notes and lists of historically significant locations! This seems to be the planning the Chief Historian was doing before he left. Perhaps these notes can be used to determine which locations to search?
+
+Throughout the Chief's office, the historically significant locations are listed not by name but by a unique number called the location ID. To make sure they don't miss anything, The Historians split into two groups, each searching the office and trying to create their own complete list of location IDs.
+
+There's just one problem: by holding the two lists up side by side (your puzzle input), it quickly becomes clear that the lists aren't very similar. Maybe you can help The Historians reconcile their lists?
+
+For example:
+
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+Maybe the lists are only off by a small amount! To find out, pair up the numbers and measure how far apart they are. Pair up the smallest number in the left list with the smallest number in the right list, then the second-smallest left number with the second-smallest right number, and so on.
+
+Within each pair, figure out how far apart the two numbers are; you'll need to add up all of those distances. For example, if you pair up a 3 from the left list with a 7 from the right list, the distance apart is 4; if you pair up a 9 with a 3, the distance apart is 6.
+
+In the example list above, the pairs and distances would be as follows:
+
+The smallest number in the left list is 1, and the smallest number in the right list is 3. The distance between them is 2.
+The second-smallest number in the left list is 2, and the second-smallest number in the right list is another 3. The distance between them is 1.
+The third-smallest number in both lists is 3, so the distance between them is 0.
+The next numbers to pair up are 3 and 4, a distance of 1.
+The fifth-smallest numbers in each list are 3 and 5, a distance of 2.
+Finally, the largest number in the left list is 4, while the largest number in the right list is 9; these are a distance 5 apart.
+To find the total distance between the left list and the right list, add up the distances between all of the pairs you found. In the example above, this is 2 + 1 + 0 + 1 + 2 + 5, a total distance of 11!
+
+Your actual left and right lists contain many location IDs. What is the total distance between your lists?
+
+
+--- Part Two ---
+
+Your analysis only confirmed what everyone feared: the two lists of location IDs are indeed very different.
+
+Or are they?
+
+The Historians can't agree on which group made the mistakes or how to read most of the Chief's handwriting, but in the commotion you notice an interesting detail: a lot of location IDs appear in both lists! Maybe the other numbers aren't location IDs at all but rather misinterpreted handwriting.
+
+This time, you'll need to figure out exactly how often each number from the left list appears in the right list. Calculate a total similarity score by adding up each number in the left list after multiplying it by the number of times that number appears in the right list.
+
+Here are the same example lists again:
+
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+For these example lists, here is the process of finding the similarity score:
+
+The first number in the left list is 3. It appears in the right list three times, so the similarity score increases by 3 * 3 = 9.
+The second number in the left list is 4. It appears in the right list once, so the similarity score increases by 4 * 1 = 4.
+The third number in the left list is 2. It does not appear in the right list, so the similarity score does not increase (2 * 0 = 0).
+The fourth number, 1, also does not appear in the right list.
+The fifth number, 3, appears in the right list three times; the similarity score increases by 9.
+The last number, 3, appears in the right list three times; the similarity score again increases by 9.
+So, for these example lists, the similarity score at the end of this process is 31 (9 + 4 + 0 + 0 + 9 + 9).
+
+Once again consider your left and right lists. What is their similarity score?
+
+"""
+
+# LIBRARIES
+import time
+import pathlib
+import os
+from math import *
+from functools import *
+from sys import *
+from collections import *
+from queue import *
+
+# MODULES
+from _test import test
+
+# OPTIONAL VARIABLES
+DISPLAY_EXTRA_INFO = True
+# DISPLAY_EXTRA_INFO = False
+
+def analyze_two_column_list_of_nums(part, input_str, DEBUG = False):
+
+  # DATA STRUCTURES
+
+  left_nums = []
+  right_nums = []
+
+  right_freq = {}
+
+
+  # PARSE INPUT DATA
+
+  input_arr = input_str.split('\n')
+  for line in input_arr:
+    [LS, RS] = [ int(n) for n in line.split('   ') ]
+
+    left_nums.append(LS)
+    right_nums.append(RS)
+
+    if RS not in right_freq: right_freq[RS] = 0
+    right_freq[RS] += 1
+
+
+  # ANALYZE
+
+  TIME_AT_START = time.time()
+
+  total = 0
+
+  if part == 1:                                     # PART 1: compare lowest left to lowest right, next lowest left to next lowest right, etc.
+
+    left_nums.sort()
+    right_nums.sort()
+
+    for i in range(len(left_nums)):
+      total += abs(left_nums[i] - right_nums[i])    # add the absolute difference in the comparison to the total
+
+  else:                                             # PART 2: for each left num, we need to know how often it appears in the right side list
+
+    for num in left_nums:
+      if num in right_freq:
+        total += right_freq[num] * num              # add the product of the num itself with its right side frequency to the total
+                                                    # (if num doesn't appear, then its freq is 0, which is a no-op with respect to the total)
+
+  return total
+
+
+# TEST CASES
+
+test_num = [1]
+test_input = None
+test_expected = None
+func = analyze_two_column_list_of_nums
+skipped_tests = set([ 2, 3, 4 ])
+skipped_tests = set([ 3, 4 ])
+skipped_tests = set([ 4 ])
+skipped_tests = set([  ])
+lowest_test = 0
+highest_test = 0
+
+FILENAME = pathlib.Path(__file__).name
+FILENAME_SPLIT = FILENAME.split('.py')[0].split('day')
+DAY_NUM = FILENAME_SPLIT[1] if len(FILENAME_SPLIT) > 1 else None
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+INPUT_PATH = CURRENT_DIR + '\\' + (f"day{DAY_NUM}-input.txt" if DAY_NUM else 'template-input.txt')
+actual_input = open(INPUT_PATH, 'r').read()
+
+sample_input = """3   4
+4   3
+2   5
+1   3
+3   9
+3   3"""
+
+# Test case 1
+test_input = {
+  'part': 1,
+  'input_str': sample_input,
+  'DEBUG': True,
+}
+test_expected = 11
+test(func, test_input, test_expected, test_num, skipped_tests, lowest_test, highest_test)
+
+# Test case 2
+test_input = {
+  'part': 1,
+  'input_str': actual_input,
+}
+test_expected = 2430334
+test(func, test_input, test_expected, test_num, skipped_tests, lowest_test, highest_test)
+
+# Test case 3
+test_input = {
+  'part': 2,
+  'input_str': sample_input,
+  'DEBUG': True,
+}
+test_expected = 31
+test(func, test_input, test_expected, test_num, skipped_tests, lowest_test, highest_test)
+
+# Test case 4
+test_input = {
+  'part': 2,
+  'input_str': actual_input,
+}
+test_expected = 28786472
+test(func, test_input, test_expected, test_num, skipped_tests, lowest_test, highest_test)
