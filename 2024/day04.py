2ba0556147607c499386ffb336cb530fec72d368
Stanley So <stanley.c.so@gmail.com>
2024-12-04 00:26:22 -0500

    Add day 4

---
+++ 2024/day04.py
@@ -0,0 +1,203 @@
+"""
+
+--- Day 4: Ceres Search ---
+
+"Looks like the Chief's not here. Next!" One of The Historians pulls out a device and pushes the only button on it. After a brief flash, you recognize the interior of the Ceres monitoring station!
+
+As the search for the Chief continues, a small Elf who lives on the station tugs on your shirt; she'd like to know if you could help her with her word search (your puzzle input). She only has to find one word: XMAS.
+
+This word search allows words to be horizontal, vertical, diagonal, written backwards, or even overlapping other words. It's a little unusual, though, as you don't merely need to find one instance of XMAS - you need to find all of them. Here are a few ways XMAS might appear, where irrelevant characters have been replaced with .:
+
+
+..X...
+.SAMX.
+.A..A.
+XMAS.S
+.X....
+The actual word search will be full of letters instead. For example:
+
+MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX
+In this word search, XMAS occurs a total of 18 times; here's the same word search again, but where letters not involved in any XMAS have been replaced with .:
+
+....XXMAS.
+.SAMXMS...
+...S..A...
+..A.A.MS.X
+XMASAMX.MM
+X.....XA.A
+S.S.S.S.SS
+.A.A.A.A.A
+..M.M.M.MM
+.X.X.XMASX
+Take a look at the little Elf's word search. How many times does XMAS appear?
+
+
+--- Part Two ---
+
+The Elf looks quizzically at you. Did you misunderstand the assignment?
+
+Looking for the instructions, you flip over the word search to find that this isn't actually an XMAS puzzle; it's an X-MAS puzzle in which you're supposed to find two MAS in the shape of an X. One way to achieve that is like this:
+
+M.S
+.A.
+M.S
+Irrelevant characters have again been replaced with . in the above diagram. Within the X, each MAS can be written forwards or backwards.
+
+Here's the same example from before, but this time all of the X-MASes have been kept instead:
+
+.M.S......
+..A..MSMS.
+.M.S.MAA..
+..A.ASMSM.
+.M.S.M....
+..........
+S.S.S.S.S.
+.A.A.A.A..
+M.M.M.M.M.
+..........
+In this example, an X-MAS appears 9 times.
+
+Flip the word search from the instructions back over to the word search side and try again. How many times does an X-MAS appear?
+
+"""
+
+# LIBRARIES
+import time
+import pathlib
+import os
+from math import *
+from functools import *
+from sys import *
+from collections import *
+from queue import *
+
+# MODULES
+from _test import test
+
+# OPTIONAL VARIABLES
+DISPLAY_EXTRA_INFO = True
+# DISPLAY_EXTRA_INFO = False
+
+def word_search(part, input_str, DEBUG = False):
+
+  # PARSE INPUT DATA
+
+  input_arr = input_str.split('\n')
+
+  H, W = len(input_arr), len(input_arr[0])
+
+
+  # ANALYZE
+
+  TIME_AT_START = time.time()
+
+  total = 0
+
+  if part == 1:                                                                                 # PART 1: search in 8 directions for 'XMAS'
+
+    DELTAS = (+1, 0), (-1, 0), (0, -1), (0, +1), (-1, -1), (-1, +1), (+1, -1), (+1, +1)
+    XMAS = 'XMAS'
+    L = len(XMAS)
+
+    for row in range(H):
+      for col in range(W):
+        for (dy, dx) in DELTAS:
+          r = row
+          c = col
+          error = False
+          for i in range(L):
+            if not ((0 <= r < H) and (0 <= c < W)) or input_arr[r][c] != XMAS[i]:
+              error = True
+              break
+            r += dy
+            c += dx
+          if not error: total += 1
+
+  else:                                                                                         # PART 2: search for 'MAS' in X shape
+
+    A = 'A'
+    MS = 'MS'
+    SM = 'SM'
+
+    for row in range(1, H - 1):                                                                 # skip outer layer...
+      for col in range(1, W - 1):                                                               # ...when searching for 'A'
+        if input_arr[row][col] == A:
+          slash = (input_arr[row - 1][col + 1] + input_arr[row + 1][col - 1]) in (MS, SM)       # i.e., make sure these cells...
+          backslash = (input_arr[row - 1][col - 1] + input_arr[row + 1][col + 1]) in (MS, SM)   # ...are 'M' and 'S' in some order
+          if slash and backslash: total += 1
+
+  return total
+
+# TEST CASES
+
+test_num = [1]
+test_input = None
+test_expected = None
+func = word_search
+skipped_tests = set([ 2, 3, 4 ])
+skipped_tests = set([ 3, 4 ])
+skipped_tests = set([ 4 ])
+skipped_tests = set([  ])
+lowest_test = 0
+highest_test = 0
+
+FILENAME = pathlib.Path(__file__).name
+FILENAME_SPLIT = FILENAME.split('.py')[0].split('day')
+DAY_NUM = FILENAME_SPLIT[1] if len(FILENAME_SPLIT) > 1 else None
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+INPUT_PATH = CURRENT_DIR + '\\' + (f"day{str(DAY_NUM).zfill(2)}-input.txt" if DAY_NUM else 'template-input.txt')
+actual_input = open(INPUT_PATH, 'r').read()
+
+sample_input = """MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX"""
+
+# Test case 1
+test_input = {
+  'part': 1,
+  'input_str': sample_input,
+  'DEBUG': True,
+}
+test_expected = 18
+test(func, test_input, test_expected, test_num, skipped_tests, lowest_test, highest_test)
+
+# Test case 2
+test_input = {
+  'part': 1,
+  'input_str': actual_input,
+}
+test_expected = 2567
+test(func, test_input, test_expected, test_num, skipped_tests, lowest_test, highest_test)
+
+# Test case 3
+test_input = {
+  'part': 2,
+  'input_str': sample_input,
+  'DEBUG': True,
+}
+test_expected = 9
+test(func, test_input, test_expected, test_num, skipped_tests, lowest_test, highest_test)
+
+# Test case 4
+test_input = {
+  'part': 2,
+  'input_str': actual_input,
+}
+test_expected = 2029
+test(func, test_input, test_expected, test_num, skipped_tests, lowest_test, highest_test)
