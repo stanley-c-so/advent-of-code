abc413fdb31197d3a7d63ffe2d639fb40031bdfe
Stanley So <stanley.c.so@gmail.com>
2024-12-31 13:22:52 -0500

    Complete part 2 after finishing the year

--- 2019/python/day25.py
+++ 2019/python/day25.py
@@ -26,6 +26,7 @@ Look around the ship and see if you can find the password for the main airlock.
 
 
 --- Part Two ---
+
 As you move through the main airlock, the air inside the ship is already heating up to reasonable levels. Santa explains that he didn't notice you coming because he was just taking a quick nap. The ship wasn't frozen; he just had the thermostat set to "North Pole".
 
 You make your way over to the navigation console. It beeps. "Status: Stranded. Please supply measurements from 49 stars to recalibrate."
@@ -35,6 +36,8 @@ You make your way over to the navigation console. It beeps. "Status: Stranded. P
 Santa just smiles and nods his head toward the window. There, in the distance, you can see the center of the Solar System: the Sun!
 
 The navigation console beeps again.
+
+If you like, you can [Align the Warp Drive again].
 
 """
 
